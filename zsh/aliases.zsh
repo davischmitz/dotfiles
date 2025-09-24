6cd820b57ede223cb4e43da69d09179c38327b79
Davi Schmitz <davi.schmitz@sap.com>
2025-09-24 12:07:41 -0300

    Add eza aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,7 @@
 
 alias k='kubectl'
 alias lzd='lazydocker'
-alias ls='eza -l --icons'
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
